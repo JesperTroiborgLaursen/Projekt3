3483ec511aefe640c3a92651b4d19a8ec2c9411e
Znaapski <znaapski@users.noreply.github.com>
2020-10-22 14:18:47 +0200

    Oprettet nyt projekt med DLL og RPI referancer der duer.

---
+++ BPMeasurer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using RaspberryPiCore.ADC;
+using RaspberryPiCore.TWIST;
+using RaspberryPiCore.LCD;
+using RPI;
+
+
+namespace BPMeasurer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            SerLCD lcd = new SerLCD();
+
+            ADC1015 adc = new ADC1015();
+            RPi rpi = new RPi();
+            adc.readADC_Differential_2_3();
+
+            ADC1115 adc2 = new
+
+
+
+        }
+    }
+}
