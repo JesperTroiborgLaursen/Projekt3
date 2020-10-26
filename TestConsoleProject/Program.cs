001ff187a0ef3c5a6875ab0278247e9d0dec91e8
Jesper Trøiborg <jespertroiborg@hotmail.com>
2020-10-26 15:57:45 +0100

    Created UDP test project. Impl. UDP sender

--- TestConsoleProject/Program.cs
+++ TestConsoleProject/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using BusinessLogicCore.Controller;
+using DataAccessLogicCore.Boundaries;
 
 namespace TestConsoleProject
 {
     class Program
     {
+        private const int PORT = 11000;
+        private const string ip = "192.168.43.255"; //Standard networking broadcast IP  
+
         static void Main(string[] args)
         {
-            
+            //BroadcastLogic broadcastLogic = new BroadcastLogic();
+
+            //BPLogic bpLogic = new BPLogic();
+
+            //broadcastLogic.BroadcastSamplePack(bpLogic.ReadAdc());
+            while (true)
+            {
+                Console.WriteLine("Ready to broadcast");
+                BroadcastMessage(Console.ReadLine());
+            }
+
+            void BroadcastMessage(String message)
+            {
+                IPAddress broadcast = IPAddress.Parse(ip);
+                Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+                byte[] sendbuf = Encoding.ASCII.GetBytes(message);
+                //byte[] sendbufhundred = new byte[100];
+                //sendbuf.CopyTo(sendbufhundred,0);
+                IPEndPoint ep = new IPEndPoint(broadcast, PORT);
+
+                s.SendTo(sendbuf, ep);
+
+                Console.WriteLine("Message sent to the broadcast address");
+            }
+
+
+
         }
     }
 }
